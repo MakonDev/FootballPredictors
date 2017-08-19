2bfdc063c34c5bd318e6b2833725a0c45cdf92d2
Nick Sadler <nicksadler14@gmail.com>
2017-08-19 09:37:07 -0400

    Neural Net added to assess best r^2 for model, need to actually run everything in the grid search because it's VERY exhaustive

--- Neural Net Code/SVR.py
+++ Neural Net Code/SVR.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
 
 '''
 Use a Support Vector Machine to perform regression on spreads to predict future ones
@@ -64,8 +65,9 @@ print("Done with data transformations.")
 '''
 Begin search for best SVR model
 '''
-
+'''
 print("Begin Support Vector Regression Grid Search of Kernels, C, Gamma & Epsilon parameters")
+
 #Create pipeline with SVR
 pipeline = Pipeline([('scl', StandardScaler()),
                     ('svr', SVR())])
@@ -73,10 +75,33 @@ pipeline = Pipeline([('scl', StandardScaler()),
 param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
 kernel_range = ['rbf','linear']
 param_grid = [{'svr__C': param_range,'svr__gamma': param_range,'svr__epsilon': param_range,'svr__kernel': kernel_range,}]
-gs = GridSearchCV(estimator=pipeline, param_grid=param_grid, scoring='r2', cv=10, verbose=2, error_score='raise', n_jobs=4)
+gs = GridSearchCV(estimator=pipeline, param_grid=param_grid, scoring='r2', cv=10, verbose=1, error_score='raise', n_jobs=4)
 gs = gs.fit(fullX, y)
 print("Best GridSearch Score: ", gs.best_score_)
 print("Best GridSearchParameters: ", gs.best_params_)
+'''
+
+
+print("Begin Neural Net Regression Grid Search of solver, activation function, hidden layer size, learning rate, alpha, momentum & max iteration parameters")
+
+#Create pipeline with mlp regressor
+pipeline = Pipeline([('scl', StandardScaler()),
+                    ('mlp', MLPRegressor(shuffle=True, random_state=1))])
+
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0]
+hidden_size_range = [1,5,10,25,50,100]
+solver_range = ['lbfgs', 'adam']
+activation_range = ['identity', 'logistic', 'tanh', 'relu']
+momentum_range = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+max_iter_range = [25,50,100,200,300,400]
+param_grid = [{'mlp__solver': solver_range,'mlp__activation': activation_range,'mlp__hidden_layer_sizes': hidden_size_range,'mlp__alpha': param_range,'mlp__learning_rate_init': param_range, 'mlp__momentum': momentum_range, 'mlp__max_iter': max_iter_range}]
+gs = GridSearchCV(estimator=pipeline, param_grid=param_grid, scoring='r2', cv=7, verbose=2, error_score='raise', n_jobs=1)
+gs = gs.fit(fullX, y)
+print("Best GridSearch Score: ", gs.best_score_)
+print("Best GridSearchParameters: ", gs.best_params_)
+
+
+
 
 '''
 After finding best model, analyze it
